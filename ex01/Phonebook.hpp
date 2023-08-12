321889c392d3002a0f49313156f7e87fb197aaf1
Laura Massonet <lamasson@2G7.42angouleme.fr>
2023-08-12 15:31:40 +0200

    0.2.1

--- ex01/Phonebook.hpp
+++ ex01/Phonebook.hpp
@@ -1,1 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lamasson <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/12 15:24:54 by lamasson          #+#    #+#             */
+/*   Updated: 2023/08/12 15:30:32 by lamasson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include "Contact.hpp"
+
+class PhoneBook {
+
+public:
+	Contact	contacts[8];
+
+	PhoneBook(void);
+	~PhoneBook(void);
+
+	void	setting_orders(void);
+
+	int		getnbcont(void) const;
+	void	setnbcont(void);
+	int		getoldcont(void) const;
+	void	setoldcont(void);
+
+private:
+	int		_nbcont;
+	int		_oldcont;
+
+	void	add(void);
+	void	search(void);
+	void	error_setting(void) const;
+};
+
+#endif
