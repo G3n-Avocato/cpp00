37b11047c6eacf2dfa1f0142188536275475f7a7
Laura Massonet <lamasson@1G1.42angouleme.fr>
2023-08-14 22:58:13 +0200

    1.6

---
+++ M01/ex05/Harl.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lamasson <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/14 14:07:33 by lamasson          #+#    #+#             */
+/*   Updated: 2023/08/14 18:20:13 by lamasson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+#include <cstring>
+
+Harl::Harl(void) {
+
+}
+
+Harl::~Harl(void) {
+
+}
+
+void	Harl::debug(void) {
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do !" << std::endl;
+}
+
+void	Harl::info(void) {
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger ! If you did, I wouldn’t be asking for more !" << std::endl;
+}
+
+void	Harl::warning(void) {
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error(void) {
+	std::cout << "This is unacceptable ! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain(std::string level) {
+	void	(Harl::*f[])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	const char	*ref[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int			i;
+
+	for (i = 0; ref[i]; i++) {
+		if (strcmp(level.c_str(), ref[i]) == 0) {	
+			(this->*f[i])();
+			break ;
+		}
+	}
+	if (i == 5)
+		std::cout << "it's not Harl's complaint ..." << std::endl;
+}
---
+++ M01/ex05/Harl.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lamasson <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/14 14:07:56 by lamasson          #+#    #+#             */
+/*   Updated: 2023/08/14 17:52:47 by lamasson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <string>
+#include <iostream>
+
+class	Harl {
+	
+	public:
+		
+		Harl(void);
+		~Harl(void);
+
+		void	complain(std::string level);
+
+	private:
+		
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+
+};
+
+#endif
